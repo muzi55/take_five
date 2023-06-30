58bf84251243d2f499f96467eacd1d3ca224d532
rmdkak <rmdkak99786@naver.com>
2023-06-30 13:01:30 +0900

    Fix: mypage coflict 해결

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,46 +1,85 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  deleteDoc,
+  getDocs,
+  query,
+  where,
+  doc,
+} from 'firebase/firestore';
 import { auth, db } from '../firebase';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { decode, encode } from 'url-safe-base64';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInfo } from '../redux/modules/UserInfo';
+import { getUserWrite } from '../redux/modules/UserWrite';
+import { getUserPhoto } from '../redux/modules/UserPhoto';
 import * as S from '../style/MypageStyled';
 
 function MyPage() {
   const navigate = useNavigate();
   const params = useParams();
-  const [item, setItem] = useState([]);
-  const [write, setWrite] = useState([]);
-  const user = auth.currentUser;
-  //로컬, 세션 스토리지를 이용한 새로고침 대응
+  const dispatch = useDispatch();
+  const userInfo = useSelector((state) => state.userInfo);
+  const userWrite = useSelector((state) => state.userWrite);
+  const userPhoto = useSelector((state) => state.userPhoto);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (users) => {
-      console.log(users);
-    });
-
-    const fetchData = async () => {
-      const dbUsers = await getDocs(collection(db, 'users'));
-      const dbWrite = await getDocs(collection(db, 'infos'));
-      const usersData = [];
-      const writeData = [];
-
-      dbUsers.forEach((doc) => {
-        const usersId = doc.data().email;
-        if (usersId === atob(decode(params.email))) usersData.push(doc.data());
-      });
-      setItem(...usersData);
-
-      dbWrite.forEach((doc) => {
-        const writeId = doc.data().email;
-        if (writeId === atob(decode(params.email))) writeData.push(doc.data());
-      });
-      setWrite([...writeData]);
-    };
-    fetchData();
+    fetchUserData();
+    fetchInfoData();
   }, []);
 
-  const logout = () => {};
+  onAuthStateChanged(auth, (users) => {
+    dispatch(getUserPhoto(users.photoURL));
+  });
+
+  const fetchUserData = async () => {
+    const dbUsers = query(
+      collection(db, 'users'),
+      where('email', '==', atob(decode(params.id)))
+    );
+
+    const usersData = [];
+
+    const userSnapshot = await getDocs(dbUsers);
+    userSnapshot.forEach((doc) => {
+      usersData.push(doc.data());
+    });
+    dispatch(getUserInfo(...usersData));
+  };
+
+  const fetchInfoData = async () => {
+    const dbWrite = query(
+      collection(db, 'infos'),
+      where('email', '==', atob(decode(params.id)))
+    );
+
+    const writeData = [];
+
+    const writeSnapshot = await getDocs(dbWrite);
+    writeSnapshot.forEach((doc) => {
+      writeData.push({ id: doc.id, ...doc.data() });
+    });
+    dispatch(getUserWrite([...writeData]));
+  };
+
+  const logout = async (event) => {
+    if (confirm('로그아웃 하시겠습니까?')) {
+      event.preventDefault();
+      await signOut(auth);
+      navigate('/');
+    }
+  };
+
+  const deleteWrite = async (id) => {
+    if (confirm('삭제 하시겠습니까?')) {
+      const writeRef = doc(db, 'infos', id);
+      await deleteDoc(writeRef);
+      fetchInfoData();
+    }
+  };
+
   return (
     <S.Layout>
       <S.Nav>
@@ -48,8 +87,7 @@ function MyPage() {
         <S.NavImgBtn
           onClick={() => navigate(`/mypage/${encode(btoa(user.email))}`)}
         >
-          {/* <S.NavImg src={user.photoURL ?? '/user.png'} alt="" /> */}
-          <S.NavImg src="/user.png" alt="" />
+          <S.NavImg src={userPhoto ?? '/user.png'} alt="" />
         </S.NavImgBtn>
       </S.Nav>
       <S.Container>
@@ -59,61 +97,39 @@ function MyPage() {
           >
             <img src="" alt="" />
           </S.EditBtn>
-          {/* <S.Img src={user.photoURL ?? '/user.png'} alt="" /> */}
-          <S.Img src="/user.png" alt="" />
+          <S.Img src={userPhoto ?? '/user.png'} alt="" />
           <S.Profile>프로필</S.Profile>
         </S.ProfileImg>
         <S.NickNameBox>
-          {item.nickname}
+          {userInfo.nickname}
           <br />
-          나의 게시물 수 : {write.length} / 게시물 좋아요 수 : ♥ {}
+          나의 게시물 수 : {userWrite.length} / 게시물 좋아요 수 : ♥{' '}
+          {userWrite.map((obj) => Number(obj.like)).reduce((a, b) => a + b, 0)}
         </S.NickNameBox>
         <S.IntroBox>
           <div>소개글</div>
-          <div>{item.introduce}</div>
+          <div>{userInfo.introduce}</div>
         </S.IntroBox>
         <S.WriteList>
           나의 게시물
-          <S.StList>
-            <S.ListTitle>
-              아무튼 그냥 테스트용 긴 텍스트
-              <S.ListBtn>♥</S.ListBtn>
-            </S.ListTitle>
-            <S.ListBtnBox>
-              <S.ListBtn onClick={() => navigate('/')}>수정</S.ListBtn>
-              <S.ListBtn>삭제</S.ListBtn>
-            </S.ListBtnBox>
-          </S.StList>
-          <S.StList>
-            <S.ListTitle>
-              text
-              <S.ListBtn>♥</S.ListBtn>
-            </S.ListTitle>
-            <S.ListBtnBox>
-              <S.ListBtn onClick={() => navigate('/')}>수정</S.ListBtn>
-              <S.ListBtn>삭제</S.ListBtn>
-            </S.ListBtnBox>
-          </S.StList>
-          <S.StList>
-            <S.ListTitle>
-              text
-              <S.ListBtn>♥</S.ListBtn>
-            </S.ListTitle>
-            <S.ListBtnBox>
-              <S.ListBtn onClick={() => navigate('/')}>수정</S.ListBtn>
-              <S.ListBtn>삭제</S.ListBtn>
-            </S.ListBtnBox>
-          </S.StList>
-          <S.StList>
-            <S.ListTitle>
-              text
-              <S.ListBtn>♥</S.ListBtn>
-            </S.ListTitle>
-            <S.ListBtnBox>
-              <S.ListBtn onClick={() => navigate('/')}>수정</S.ListBtn>
-              <S.ListBtn>삭제</S.ListBtn>
-            </S.ListBtnBox>
-          </S.StList>
+          {userWrite.map((obj) => {
+            return (
+              <S.StList key={obj.id}>
+                <S.ListTitle>
+                  {obj.email}
+                  <S.ListBtn>♥</S.ListBtn>
+                </S.ListTitle>
+                <S.ListBtnBox>
+                  <S.ListBtn onClick={() => navigate(`/editdetail/${obj.id}`)}>
+                    수정
+                  </S.ListBtn>
+                  <S.ListBtn onClick={() => deleteWrite(obj.id)}>
+                    삭제
+                  </S.ListBtn>
+                </S.ListBtnBox>
+              </S.StList>
+            );
+          })}
         </S.WriteList>
       </S.Container>
     </S.Layout>
