80b20c7de96cca4ae7c5da567d854a0a467354d0
muzi55 <muzi5@kakao.com>
2023-06-30 13:54:09 +0900

    feat: 좋아요 버튼 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { collection, deleteDoc, doc, getDocs, query } from 'firebase/firestore';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+} from 'firebase/firestore';
+
 import { db } from '../firebase';
 import { InnerBox } from './Write';
 import { MyInfo, WriteBox } from '../style/DetailStyled';
 
 import { useParams } from 'react-router-dom';
+import LikeImg from '../images/Like.svg';
+import styled from 'styled-components';
 
 function Detail() {
   const param = useParams();
@@ -12,6 +22,8 @@ function Detail() {
   const paramId = param.email.split('&')[1];
 
   const [userInfo, setUserInfo] = useState({});
+  const [info, setInfo] = useState({});
+
   // 데이터 읽기 -----------------------------------------------------
   useEffect(() => {
     const fetchData = async () => {
@@ -35,18 +47,15 @@ function Detail() {
         initialUsers.push({ id: doc.id, ...doc.data() });
       });
 
-      // console.log(initialInfos);
-      console.log(initialUsers);
-
       const filterInfo = initialInfos.filter((info) => {
         if (info.email === paramEmail) {
           return info;
         }
       });
-
       initialUsers.filter((user) => {
         if (user.email === paramEmail) {
           setUserInfo({ ...user, ...filterInfo[0] });
+          setInfo(filterInfo[0]);
         }
       });
     };
@@ -60,21 +69,39 @@ function Detail() {
   //   // bucket 콜렉션의 bucket_item 문서 삭제
   //   bucket.doc(userInfo.id).delete();
   // })
-  // console.log(userInfo.id);
+
+  // 이부분이 위로 올라가야 업데이트가 원활히 됩니다.
+  const { company, goodBad, grow, introduce, like, motive, name, skill } =
+    userInfo;
+
   const deleteInfo = async (event) => {
     if (confirm('삭제하시겠습니까?')) {
-      const todoRef = doc(db, 'infos', userInfo.id);
+      const todoRef = doc(db, 'infos', like);
       await deleteDoc(todoRef);
     }
   };
 
-  const { company, goodBad, grow, introduce, like, motive, name, skill } =
-    userInfo;
-  console.log(userInfo);
+  // 업데이트 부분
+  const [render, setRender] = useState(like);
+  const updateInfo = async (event) => {
+    const infoRef = doc(db, 'infos', info.id);
+    // 기존값, {...info, 변경해야할키 : 변경해야하는값}
+    await updateDoc(infoRef, { ...info, like: Number(like) + 1 }); // 업데이트할 필드 명시
+
+    setRender((render) => render + 1);
+  };
+  // console.log(userInfo);
+
   return (
     <InnerBox>
       {/* my page 내용 */}
       <MyInfo>
+        {/* 추가부분 라이크 박스 */}
+        <StLikeSpan>
+          <img onClick={updateInfo} src={LikeImg} alt="하트모양 이미지" /> :
+          {render}
+          {like}
+        </StLikeSpan>
         <img
           src="https://velog.velcdn.com/images/seul-bean/profile/259fe091-ca51-424b-bf1a-aca4da376a9c/social_profile.png"
           alt="프로필 사진"
@@ -121,3 +148,21 @@ function Detail() {
 }
 
 export default Detail;
+
+const StLikeSpan = styled.span`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 2.5rem;
+  right: 4rem;
+  font-size: 1.4rem;
+  & img {
+    background: none;
+    transition: all 8s;
+    cursor: pointer;
+    &:active {
+      transform: rotateY(18560deg);
+      background: magenta;
+    }
+  }
+`;
